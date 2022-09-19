7db9d0532e1b87225779c5fb0f8840331e744eea
MugheezAsif <m.mogheesasif@gmail.com>
2022-09-20 03:14:18 +0500

    added post contriller actions empty

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+    def index 
+    end
+
+    def show 
+    end
+
+    def new 
+    end
+
+    def edit 
+    end
+
+    def create 
+    end
+
+    def update 
+    end
+
+    def destroy 
+    end
+
+    private 
+
+    
 end
