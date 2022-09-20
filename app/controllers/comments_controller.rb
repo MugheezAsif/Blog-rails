fbcda901df83167389610a64bb96aa9933a9a069
MugheezAsif <m.mogheesasif@gmail.com>
2022-09-20 18:29:08 +0500

    Completed all the views and controller part of Post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,18 @@
 class CommentsController < ApplicationController
     def new 
+        @comment = Comment.new
     end
 
     def edit 
     end 
 
     def destroy 
+        @comment.destroy
+
+        respond_to do |format|
+            format.html { redirect_to posts_url, notice: "Comment was successfully destroyed." }
+            format.json { head :no_content }
+        end
     end
 
     private 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,20 +12,24 @@ class PostsController < ApplicationController
     end
 
     def edit 
+        @post = Post.find(params[:id])
     end
 
     def create 
         @post = Post.new(post_params)
-        if @post.save
-            format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-            format.json { render :show, status: :created, location: @post }
-        else
-            format.html { render :new, status: :unprocessable_entity }
-            format.json { render json: @post.errors, status: :unprocessable_entity }
+        respond_to do |format|
+            if @post.save
+                format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+                format.json { render :show, status: :created, location: @post }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @post.errors, status: :unprocessable_entity }
+            end
         end
     end
 
     def update 
+        @post = Post.find(params[:id])
         respond_to do |format|
             if @post.update(post_params)
               format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
@@ -38,7 +42,7 @@ class PostsController < ApplicationController
     end
 
     def destroy 
-        @user.destroy
+        @post.destroy
 
         respond_to do |format|
             format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
